c89b764eb1be98feeb3df4a907b25d455943a37b
franjavi06 <franjavi06@gmail.com>
2022-02-18 17:28:56 -0400

    test2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="vote" element={<Vote />} />
+      </Routes>
     </div>
   );
 }
